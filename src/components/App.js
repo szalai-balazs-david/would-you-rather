b0e8dfd80ea5f53609840011e86dc5e7a856a496
Balazs <balazs@altaeros.com>
2020-09-13 18:39:58 -0400

    Add dummy LoginPage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
+import LoginPage from './LoginPage'
 
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialData())
   }
+
   render(){
+    const {authenticated} = this.props
+
+    if(!authenticated){
+      return <LoginPage />
+    }
+
     return (
       <div>
         App
@@ -15,4 +23,10 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+function mapStateToProps({authedUser}){
+  return {
+    authenticated: authedUser !== null
+  }
+}
+
+export default connect(mapStateToProps)(App)
