dfe18e25b27322c661985f45c80bccfb4ce6dd6c
Balazs <balazs@altaeros.com>
2020-09-15 19:39:56 -0400

    Add more content to Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
 class Question extends Component {
   render(){
+    const {question, author} = this.props
     return (
       <div>
-        Question {this.props.id}
+        <p>{author.name} asks:</p>
+        <p>Would you rather...</p>
+        <p>{question.optionOne.text}</p>
+        <p>{question.optionTwo.text}</p>
       </div>
     )
   }
 }
 
-export default Question
+function mapStateToProps({users, questions}, {id}){
+  const question = questions[id]
+  const author = users[question.author]
+  return {
+    author,
+    question
+  }
+}
+
+export default connect(mapStateToProps)(Question)
